faefac76c9306edbc9cdcee74be2aa4419101458
jerry <1246286991@qq.com>
2018-11-17 15:32:54 +0800

    第二次修改

--- js/member.js
+++ js/member.js
@@ -2,140 +2,169 @@ var member = [
     {
         "index": "1",
         "trueSrc":"img/baowenbei.png",
-        "src":"img/baowenbei_mini.png"
+        "src":"img/baowenbei_mini.png",
+        "desc":"保温杯：冬天帮你保住水温哦"
     },
     {
         "index": "2",
         "trueSrc":"img/hufu.png",
-        "src":"img/hufu_mini.png"
+        "src":"img/hufu_mini.png",
+        "desc":"防晒霜：嘿嘿，让太阳离你远点"
     },
     {
         "index": "3",
         "trueSrc":"img/scarf.png",
-        "src":"img/scarf_mini.png"
+        "src":"img/scarf_mini.png",
+        "desc":"围巾：温暖你的冬日"
     },
     {
         "index": "4",
         "trueSrc":"img/shoes.png",
-        "src":"img/shoes_mini.png"
+        "src":"img/shoes_mini.png",
+        "desc":"回力小白鞋：搭配简易哦"
     },
     {
         "index": "5",
         "trueSrc":"img/shoutibao.png",
-        "src":"img/shoutibao_mini.png"
+        "src":"img/shoutibao_mini.png",
+        "desc":"手提包：出门好看，带东西也方便"
     },
     {
         "index": "6",
         "trueSrc":"img/UM.png",
-        "src":"img/UM_mini.png"
+        "src":"img/UM_mini.png",
+        "desc":"雨伞：帮你遮风挡雨哦"
     },
     {
         "index": "7",
         "trueSrc":"img/xiangshui.png",
-        "src":"img/xiangshui_mini.png"
-    }/*,
-    {
-        "index": "1",
-        "src":"img/baowenbei.png"
+        "src":"img/xiangshui_mini.png",
+        "desc":"香水礼盒：喵喵喵？？"
     },
     {
         "index": "1",
-        "src":"img/baowenbei.png"
+        "trueSrc":"img/baowenbei.png",
+        "src":"img/baowenbei_mini.png",
+        "desc":"保温杯：冬天帮你保住水温哦"
+    },
+    {
+        "index": "2",
+        "trueSrc":"img/hufu.png",
+        "src":"img/hufu_mini.png",
+        "desc":"防晒霜：嘿嘿，让太阳离你远点"
+    },
+    {
+        "index": "3",
+        "trueSrc":"img/scarf.png",
+        "src":"img/scarf_mini.png",
+        "desc":"围巾：温暖你的冬日"
+    },
+    {
+        "index": "4",
+        "trueSrc":"img/shoes.png",
+        "src":"img/shoes_mini.png",
+        "desc":"回力小白鞋：搭配简易哦"
+    },
+    {
+        "index": "5",
+        "trueSrc":"img/shoutibao.png",
+        "src":"img/shoutibao_mini.png",
+        "desc":"手提包：出门好看，带东西也方便"
+    },
+    {
+        "index": "6",
+        "trueSrc":"img/UM.png",
+        "src":"img/UM_mini.png",
+        "desc":"雨伞：帮你遮风挡雨哦"
+    },
+    {
+        "index": "7",
+        "trueSrc":"img/xiangshui.png",
+        "src":"img/xiangshui_mini.png",
+        "desc":"香水礼盒：喵喵喵？？"
     },
     {
         "index": "1",
-        "src":"img/baowenbei.png"
+        "trueSrc":"img/baowenbei.png",
+        "src":"img/baowenbei_mini.png",
+        "desc":"保温杯：冬天帮你保住水温哦"
     },
     {
         "index": "2",
-        "src":"img/hufu.png"
+        "trueSrc":"img/hufu.png",
+        "src":"img/hufu_mini.png",
+        "desc":"防晒霜：嘿嘿，让太阳离你远点"
+    },
+    {
+        "index": "3",
+        "trueSrc":"img/scarf.png",
+        "src":"img/scarf_mini.png",
+        "desc":"围巾：温暖你的冬日"
+    },
+    {
+        "index": "4",
+        "trueSrc":"img/shoes.png",
+        "src":"img/shoes_mini.png",
+        "desc":"回力小白鞋：搭配简易哦"
+    },
+    {
+        "index": "5",
+        "trueSrc":"img/shoutibao.png",
+        "src":"img/shoutibao_mini.png",
+        "desc":"手提包：出门好看，带东西也方便"
+    },
+    {
+        "index": "6",
+        "trueSrc":"img/UM.png",
+        "src":"img/UM_mini.png",
+        "desc":"雨伞：帮你遮风挡雨哦"
+    },
+    {
+        "index": "7",
+        "trueSrc":"img/xiangshui.png",
+        "src":"img/xiangshui_mini.png",
+        "desc":"香水礼盒：喵喵喵？？"
+    },
+    {
+        "index": "1",
+        "trueSrc":"img/baowenbei.png",
+        "src":"img/baowenbei_mini.png",
+        "desc":"保温杯：冬天帮你保住水温哦"
     },
     {
         "index": "2",
-        "src":"img/hufu.png"
-    },
-    {
-        "index": "2",
-        "src":"img/scarf.png"
-    },
-    {
-        "index": "3",
-        "src":"img/scarf.png"
+        "trueSrc":"img/hufu.png",
+        "src":"img/hufu_mini.png",
+        "desc":"防晒霜：嘿嘿，让太阳离你远点"
     },
     {
         "index": "3",
-        "src":"img/scarf.png"
-    },
-    {
-        "index": "3",
-        "src":"img/scarf.png"
-    },
-    {
-        "index": "3",
-        "src":"img/scarf.png"
+        "trueSrc":"img/scarf.png",
+        "src":"img/scarf_mini.png",
+        "desc":"围巾：温暖你的冬日"
     },
     {
         "index": "4",
-        "src":"img/shoes.png"
+        "trueSrc":"img/shoes.png",
+        "src":"img/shoes_mini.png",
+        "desc":"回力小白鞋：搭配简易哦"
     },
     {
         "index": "5",
-        "src":"img/shoutibao.png"
+        "trueSrc":"img/shoutibao.png",
+        "src":"img/shoutibao_mini.png",
+        "desc":"手提包：出门好看，带东西也方便"
     },
     {
         "index": "6",
-        "src":"img/UM.png"
+        "trueSrc":"img/UM.png",
+        "src":"img/UM_mini.png",
+        "desc":"雨伞：帮你遮风挡雨哦"
     },
     {
         "index": "7",
-        "src":"img/xiangshui.png"
-    },
-    {
-        "index": "4",
-        "src":"img/shoes.png"
-    },
-    {
-        "index": "5",
-        "src":"img/shoutibao.png"
-    },
-    {
-        "index": "6",
-        "src":"img/UM.png"
-    },
-    {
-        "index": "7",
-        "src":"img/xiangshui.png"
-    },
-    {
-        "index": "4",
-        "src":"img/shoes.png"
-    },
-    {
-        "index": "5",
-        "src":"img/shoutibao.png"
-    },
-    {
-        "index": "6",
-        "src":"img/UM.png"
-    },
-    {
-        "index": "7",
-        "src":"img/xiangshui.png"
-    },
-    {
-        "index": "4",
-        "src":"img/shoes.png"
-    },
-    {
-        "index": "5",
-        "src":"img/shoutibao.png"
-    },
-    {
-        "index": "6",
-        "src":"img/UM.png"
-    },
-    {
-        "index": "7",
-        "src":"img/xiangshui.png"
-    }*/
+        "trueSrc":"img/xiangshui.png",
+        "src":"img/xiangshui_mini.png",
+        "desc":"香水礼盒：喵喵喵？？"
+    }
 ];
