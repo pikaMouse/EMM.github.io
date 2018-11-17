e14e9234c8316e98be7997898b1cacbc0342dc7d
jerry <1246286991@qq.com>
2018-11-17 15:35:02 +0800

    第二次修改

--- js/member.js
+++ js/member.js
@@ -166,5 +166,89 @@ var member = [
         "trueSrc":"img/xiangshui.png",
         "src":"img/xiangshui_mini.png",
         "desc":"香水礼盒：喵喵喵？？"
+    },
+    {
+        "index": "1",
+        "trueSrc":"img/baowenbei.png",
+        "src":"img/baowenbei_mini.png",
+        "desc":"保温杯：冬天帮你保住水温哦"
+    },
+    {
+        "index": "2",
+        "trueSrc":"img/hufu.png",
+        "src":"img/hufu_mini.png",
+        "desc":"防晒霜：嘿嘿，让太阳离你远点"
+    },
+    {
+        "index": "3",
+        "trueSrc":"img/scarf.png",
+        "src":"img/scarf_mini.png",
+        "desc":"围巾：温暖你的冬日"
+    },
+    {
+        "index": "4",
+        "trueSrc":"img/shoes.png",
+        "src":"img/shoes_mini.png",
+        "desc":"回力小白鞋：搭配简易哦"
+    },
+    {
+        "index": "5",
+        "trueSrc":"img/shoutibao.png",
+        "src":"img/shoutibao_mini.png",
+        "desc":"手提包：出门好看，带东西也方便"
+    },
+    {
+        "index": "6",
+        "trueSrc":"img/UM.png",
+        "src":"img/UM_mini.png",
+        "desc":"雨伞：帮你遮风挡雨哦"
+    },
+    {
+        "index": "7",
+        "trueSrc":"img/xiangshui.png",
+        "src":"img/xiangshui_mini.png",
+        "desc":"香水礼盒：喵喵喵？？"
+    },
+    {
+        "index": "1",
+        "trueSrc":"img/baowenbei.png",
+        "src":"img/baowenbei_mini.png",
+        "desc":"保温杯：冬天帮你保住水温哦"
+    },
+    {
+        "index": "2",
+        "trueSrc":"img/hufu.png",
+        "src":"img/hufu_mini.png",
+        "desc":"防晒霜：嘿嘿，让太阳离你远点"
+    },
+    {
+        "index": "3",
+        "trueSrc":"img/scarf.png",
+        "src":"img/scarf_mini.png",
+        "desc":"围巾：温暖你的冬日"
+    },
+    {
+        "index": "4",
+        "trueSrc":"img/shoes.png",
+        "src":"img/shoes_mini.png",
+        "desc":"回力小白鞋：搭配简易哦"
+    },
+    {
+        "index": "5",
+        "trueSrc":"img/shoutibao.png",
+        "src":"img/shoutibao_mini.png",
+        "desc":"手提包：出门好看，带东西也方便"
+    },
+    {
+        "index": "6",
+        "trueSrc":"img/UM.png",
+        "src":"img/UM_mini.png",
+        "desc":"雨伞：帮你遮风挡雨哦"
+    },
+    {
+        "index": "7",
+        "trueSrc":"img/xiangshui.png",
+        "src":"img/xiangshui_mini.png",
+        "desc":"香水礼盒：喵喵喵？？"
     }
 ];
