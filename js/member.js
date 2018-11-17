9ecce70aa1a00dea824248c1941e7d3a04d9726f
jerry <1246286991@qq.com>
2018-11-17 16:03:34 +0800

    第二次修改

--- js/member.js
+++ js/member.js
@@ -9,36 +9,36 @@ var member = [
         "index": "2",
         "trueSrc":"img/hufu.png",
         "src":"img/hufu_mini.png",
-        "desc":"防晒霜：嘿嘿，让太阳离你远点"
+        "desc":"防晒霜：让太阳远远的"
     },
     {
         "index": "3",
         "trueSrc":"img/scarf.png",
         "src":"img/scarf_mini.png",
-        "desc":"围巾：温暖你的冬日"
+        "desc":"围巾：冷吗？来，让它抱一下"
     },
     {
         "index": "4",
         "trueSrc":"img/shoes.png",
         "src":"img/shoes_mini.png",
-        "desc":"回力小白鞋：搭配简易哦"
+        "desc":"回力小白鞋（36码）：搭配简易哦"
     },
     {
         "index": "5",
         "trueSrc":"img/shoutibao.png",
         "src":"img/shoutibao_mini.png",
-        "desc":"手提包：出门好看，带东西也方便"
+        "desc":"手提包：出门也要美美哒"
     },
     {
         "index": "6",
         "trueSrc":"img/UM.png",
         "src":"img/UM_mini.png",
-        "desc":"雨伞：帮你遮风挡雨哦"
+        "desc":"伞：遮阳挡雨的小助手"
     },
     {
         "index": "7",
         "trueSrc":"img/xiangshui.png",
         "src":"img/xiangshui_mini.png",
-        "desc":"香水礼盒：喵喵喵？？"
+        "desc":"香水礼盒：“香”信你的力量，成就你的梦想"
     }
 ];
