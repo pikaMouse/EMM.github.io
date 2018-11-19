c79928ab54e2b8322e41df5bdb47fa4dd56719a3
117sparta <1246286991@qq.com>
2018-11-19 19:31:50 +0800

    第二次修改

--- js/member.js
+++ js/member.js
@@ -40,5 +40,173 @@ var member = [
         "trueSrc":"img/xiangshui.png",
         "src":"img/xiangshui_mini.png",
         "desc":"香水礼盒：“香”信你的力量，成就你的梦想"
+    },
+    {
+        "index": "1",
+        "trueSrc":"img/baowenbei.png",
+        "src":"img/baowenbei_mini.png",
+        "desc":"保温杯:寒冬里给你最漫长的温暖"
+    },
+    {
+        "index": "2",
+        "trueSrc":"img/hufu.png",
+        "src":"img/hufu_mini.png",
+        "desc":"防晒霜：让太阳远远的"
+    },
+    {
+        "index": "3",
+        "trueSrc":"img/scarf.png",
+        "src":"img/scarf_mini.png",
+        "desc":"围巾：冷吗？来，让它抱一下"
+    },
+    {
+        "index": "4",
+        "trueSrc":"img/shoes.png",
+        "src":"img/shoes_mini.png",
+        "desc":"回力小白鞋（36码）：搭配简易哦"
+    },
+    {
+        "index": "5",
+        "trueSrc":"img/shoutibao.png",
+        "src":"img/shoutibao_mini.png",
+        "desc":"手提包：出门也要美美哒"
+    },
+    {
+        "index": "6",
+        "trueSrc":"img/UM.png",
+        "src":"img/UM_mini.png",
+        "desc":"伞：遮阳挡雨的小助手"
+    },
+    {
+        "index": "7",
+        "trueSrc":"img/xiangshui.png",
+        "src":"img/xiangshui_mini.png",
+        "desc":"香水礼盒：“香”信你的力量，成就你的梦想"
+    },
+    {
+        "index": "1",
+        "trueSrc":"img/baowenbei.png",
+        "src":"img/baowenbei_mini.png",
+        "desc":"保温杯:寒冬里给你最漫长的温暖"
+    },
+    {
+        "index": "2",
+        "trueSrc":"img/hufu.png",
+        "src":"img/hufu_mini.png",
+        "desc":"防晒霜：让太阳远远的"
+    },
+    {
+        "index": "3",
+        "trueSrc":"img/scarf.png",
+        "src":"img/scarf_mini.png",
+        "desc":"围巾：冷吗？来，让它抱一下"
+    },
+    {
+        "index": "4",
+        "trueSrc":"img/shoes.png",
+        "src":"img/shoes_mini.png",
+        "desc":"回力小白鞋（36码）：搭配简易哦"
+    },
+    {
+        "index": "5",
+        "trueSrc":"img/shoutibao.png",
+        "src":"img/shoutibao_mini.png",
+        "desc":"手提包：出门也要美美哒"
+    },
+    {
+        "index": "6",
+        "trueSrc":"img/UM.png",
+        "src":"img/UM_mini.png",
+        "desc":"伞：遮阳挡雨的小助手"
+    },
+    {
+        "index": "7",
+        "trueSrc":"img/xiangshui.png",
+        "src":"img/xiangshui_mini.png",
+        "desc":"香水礼盒：“香”信你的力量，成就你的梦想"
+    },
+    {
+        "index": "1",
+        "trueSrc":"img/baowenbei.png",
+        "src":"img/baowenbei_mini.png",
+        "desc":"保温杯:寒冬里给你最漫长的温暖"
+    },
+    {
+        "index": "2",
+        "trueSrc":"img/hufu.png",
+        "src":"img/hufu_mini.png",
+        "desc":"防晒霜：让太阳远远的"
+    },
+    {
+        "index": "3",
+        "trueSrc":"img/scarf.png",
+        "src":"img/scarf_mini.png",
+        "desc":"围巾：冷吗？来，让它抱一下"
+    },
+    {
+        "index": "4",
+        "trueSrc":"img/shoes.png",
+        "src":"img/shoes_mini.png",
+        "desc":"回力小白鞋（36码）：搭配简易哦"
+    },
+    {
+        "index": "5",
+        "trueSrc":"img/shoutibao.png",
+        "src":"img/shoutibao_mini.png",
+        "desc":"手提包：出门也要美美哒"
+    },
+    {
+        "index": "6",
+        "trueSrc":"img/UM.png",
+        "src":"img/UM_mini.png",
+        "desc":"伞：遮阳挡雨的小助手"
+    },
+    {
+        "index": "7",
+        "trueSrc":"img/xiangshui.png",
+        "src":"img/xiangshui_mini.png",
+        "desc":"香水礼盒：“香”信你的力量，成就你的梦想"
+    },
+    {
+        "index": "1",
+        "trueSrc":"img/baowenbei.png",
+        "src":"img/baowenbei_mini.png",
+        "desc":"保温杯:寒冬里给你最漫长的温暖"
+    },
+    {
+        "index": "2",
+        "trueSrc":"img/hufu.png",
+        "src":"img/hufu_mini.png",
+        "desc":"防晒霜：让太阳远远的"
+    },
+    {
+        "index": "3",
+        "trueSrc":"img/scarf.png",
+        "src":"img/scarf_mini.png",
+        "desc":"围巾：冷吗？来，让它抱一下"
+    },
+    {
+        "index": "4",
+        "trueSrc":"img/shoes.png",
+        "src":"img/shoes_mini.png",
+        "desc":"回力小白鞋（36码）：搭配简易哦"
+    },
+    {
+        "index": "5",
+        "trueSrc":"img/shoutibao.png",
+        "src":"img/shoutibao_mini.png",
+        "desc":"手提包：出门也要美美哒"
+    },
+    {
+        "index": "6",
+        "trueSrc":"img/UM.png",
+        "src":"img/UM_mini.png",
+        "desc":"伞：遮阳挡雨的小助手"
+    },
+    {
+        "index": "7",
+        "trueSrc":"img/xiangshui.png",
+        "src":"img/xiangshui_mini.png",
+        "desc":"香水礼盒：“香”信你的力量，成就你的梦想"
     }
 ];
